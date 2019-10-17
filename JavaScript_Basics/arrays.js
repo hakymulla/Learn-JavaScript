534e3d30346b30164978580ec3c8cd7b93d18a47
bolajiscott <bolajiscott@yahoo.com>
2019-10-17 13:41:38 +0100

    Added new methods to array

--- JavaScript_Basics/arrays.js
+++ JavaScript_Basics/arrays.js
@@ -25,3 +25,29 @@ easyMethod.push("Swapnil Satish Shinde", 76, true); // This way you can push Mul
 
 console.log(easyMethod);
 // Output [ 'Swapnil Satish Shinde', 76, true ]
+
+const b = ["hii", 26, "london"];
+a.pop();   // Removes the last element ("london") from b
+//Output ["hiii",26];
+// The pop() method returns the value that was "popped out":
+
+const b = ["hii", 26, "london"];
+a.shift(); // Removes the first element "hii" from b
+//Output [26,"london"];
+//The shift() method returns the string that was "shifted out":
+
+const b = ["hii", 26, "london"];
+b.length
+//Output 3
+// The length calls out the total numbers of collections in the array
+
+const b = ["hii", 26, "london"];
+delete b[0]
+//output true
+// Deleting elements leaves undefined holes in an array
+
+
+
+
+
+
